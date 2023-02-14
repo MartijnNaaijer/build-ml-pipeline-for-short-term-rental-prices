6c142aa140cf16a996479ef1102c7fffa897561c
Martijn N <martijn.naayer@chello.nl>
2023-02-14 14:58:53 +0100

    added geolocation to basic cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,6 +30,9 @@ def go(args):
     idx = df["price"].between(args.min_price, max_price)
     df = df[idx].copy()
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info('Save cleaned data.')
     df.to_csv(args.output_artifact, index=False)
 
